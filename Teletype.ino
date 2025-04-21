c5d209575769fd27b159a63f005f92380cf105e4
dxeoane <dseoane@gmail.com>
2025-04-21 20:46:51 +0200

    Añadir mqtt

--- Teletype.ino
+++ Teletype.ino
@@ -1,5 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>
+#include <PubSubClient.h>
+
 #include "Screen.h"
 #include "Buttons.h"
 #include "Utils.h"
@@ -10,16 +12,34 @@
 #define PORT 8888
 #define CHANNELS 3
 
+#define MQTT_ENABLED
+#define MQTT_MAX_BUFFER_SIZE 4096
+
 unsigned long lastCheckMillis = 0;
 char ACK[] = "ACK\r\n";  
 
+enum Mode {
+  TELETYPE,
+  LOGGER
+};
+
+Mode mode = TELETYPE;  
+int activeChannel = 0;
+
 WiFiUDP Udp;
+
+#ifdef MQTT_ENABLED
+WiFiClient wifiClient;
+PubSubClient mqttClient(wifiClient);
+#endif
 
 void setup() {
   char spinner[] = "-\\|/";
   
   delay(1000);
   Serial.begin(115200);  
+
+  randomSeed(micros());
 
   setupButtons();  
   setupScreen(); 
@@ -88,20 +108,27 @@ void setup() {
 
   Serial.printf("Display: %dX%d px\n", SCREEN_WIDTH, SCREEN_HEIGHT);   
   printF("Display: %dX%d px\n", SCREEN_WIDTH, SCREEN_HEIGHT);  
+
+#ifdef MQTT_ENABLED
+  Serial.println("Conectando con el servidor MQTT ...");
+  mqttClient.setServer(MQTT_SERVER, MQTT_PORT);
+  mqttClient.setCallback(mqttReceive);
+  String mqttClientId = "Teletype-";
+  mqttClientId += String(random(0xffff), HEX);
+  if (mqttClient.connect(mqttClientId.c_str(), MQTT_USER, MQTT_PASSWORD)) {
+      Serial.printf("MQTT Server: %s\n", MQTT_SERVER);
+      if (mqttClient.subscribe(MQTT_TOPIC)) {
+        Serial.printf("MQTT Topic: %s\n", MQTT_TOPIC);
+        printF("MQTT Topic: %s\n", MQTT_TOPIC);
+      }
+  }
+#endif  
   
   setBWColors(); 
   setTextSize(2);  
 }
 
 void loop() {  
-  
-  enum Mode {
-    TELETYPE,
-    LOGGER
-  };
-  
-  static Mode mode = TELETYPE;
-  static int activeChannel = 0;
 
   uint8_t pressed = changedButtons();
 
@@ -132,9 +159,7 @@ void loop() {
         initLogger();   
         break;
     }
-  }
-  
-  static char buffer[UDP_TX_PACKET_MAX_SIZE + 1];  
+  } 
 
   // Comprobamos si la WiFi sigue conectada y funcionando
   if (WiFi.getMode() == WIFI_STA) {
@@ -142,15 +167,29 @@ void loop() {
     if (WiFi.status() != WL_CONNECTED) return;
   }
 
+#ifdef MQTT_ENABLED
+  mqttClient.loop();
+#endif
+  
+  static char buffer[UDP_TX_PACKET_MAX_SIZE + 1];  
   // Recibimos un paquete UDP
   int size = Udp.parsePacket();
   if (!size) return;
 
   // Si acabamos de recibir un paquete UDP damos por supuesto que la WiFi funciona bien
-  lastCheckMillis = millis(); 
-  
-  buffer[Udp.read(buffer, UDP_TX_PACKET_MAX_SIZE)] = 0;
+  lastCheckMillis = millis();   
+  buffer[Udp.read(buffer, UDP_TX_PACKET_MAX_SIZE)] = 0;  
+  parseBuffer(buffer);
+  sendACK();
+}
 
+void sendACK() {
+  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+  Udp.write(ACK);
+  Udp.endPacket();
+}
+
+void parseBuffer(char buffer[]) {
   // Canal al que van los comandos, si no se especifica es 0
   int commandChannel = 0;
 
@@ -242,14 +281,6 @@ void loop() {
     
     line = strtok_r(NULL, "\n\r", &lasts);
   }
-
-  sendACK();
-}
-
-void sendACK() {
-  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
-  Udp.write(ACK);
-  Udp.endPacket();
 }
 
 WiFiClient client;
@@ -263,8 +294,13 @@ void checkWiFi() {
   if (WiFi.status() == WL_CONNECTED) {
     if (client.connect("192.168.10.1", 80)) {
       client.stop();
+#ifdef MQTT_ENABLED      
+      if (!mqttClient.connected()) {
+        mqttReconnect();
+      }
+#endif
       return;
-    }
+    }   
     client.stop();
   }
   
@@ -292,6 +328,40 @@ void checkWiFi() {
  
   lastCheckMillis = now;
 }
+
+#ifdef MQTT_ENABLED
+void mqttReceive(char* topic, byte* payload, unsigned int length) {
+
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.println("]");
+
+  static char buffer[MQTT_MAX_BUFFER_SIZE + 1];  
+
+  unsigned int i;
+  for (i = 0; i < MQTT_MAX_BUFFER_SIZE && i < length && payload[i]; i++)
+      buffer[i] = (char) payload[i];
+  buffer[i] = '\0';
+  
+  parseBuffer(buffer);  
+}
+
+void mqttReconnect() {
+  if (!mqttClient.connected()) {
+    Serial.println("Volviendo a conectar con el servidor MQTT ...");
+    String mqttClientId = "Teletype-";
+    mqttClientId += String(random(0xffff), HEX);
+    if (mqttClient.connect(mqttClientId.c_str(), MQTT_USER, MQTT_PASSWORD)) {
+        Serial.printf("MQTT Server: %s\n", MQTT_SERVER);
+        if (mqttClient.subscribe(MQTT_TOPIC)) {
+          Serial.printf("MQTT Topic: %s\n", MQTT_TOPIC);
+        }
+    } else {
+      Serial.println("Error al conectar con el servidor MQTT");
+    }
+  }
+}
+#endif
 
 void initTeletype() {   
   setBWColors();   
